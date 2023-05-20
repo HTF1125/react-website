88fafa31fd4c93206ad0f9ad242d92dadf408897
RobertHan <hantianfeng@outlook.com>
2023-05-20 17:51:25 +0900

    2023-5-20

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { BsArrowLeftShort } from "react-icons/bs";
+import { AiFillEnvironment } from "react-icons/ai";
+import { useState } from "react";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <div
+        className={`bg-dark-purple h-screen p-5 pt-8 ${sidebarOpen ? "w-72" : "w-20"
+          } duration-300 text-white relative`}
+      >
+        <BsArrowLeftShort
+          className={`bg-white 
+          text-dark-purple 
+          text-3xl rounded-full 
+          absolute -right-3 top-9 
+          border border-dark-purple cursor-pointer ${!sidebarOpen && "rotate-180"
+            } duration-300`}
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+        />
+
+        <div className="inline-flex">
+          <AiFillEnvironment
+            className={`bg-amber-300 text-4xl mr-2 rounded cursor-pointer block float-left duration-500 ${sidebarOpen && "rotate-[360deg]"
+              }`}
+          />
+          <h1
+            className={`text-white origin-left font-medium text-2xl duration-300 ${!sidebarOpen && "scale-0"
+              }`}
+          >
+            Tailwind
+          </h1>
+        </div>
+      </div>
+      <div className="bg-light-white p-7 text-2xl font-bold">
+        <h1 className="">HomePage</h1>
+      </div>
     </div>
   );
 }
